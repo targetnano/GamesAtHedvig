e09d9d6b12b6336d40d18aa0ae5bef1a32094995
Kaviarasu Govindaraju <kavi@hedviginc.com>
2015-01-05 15:48:38 -0800

    Min edit distance

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -2,6 +2,13 @@
 public class EditDistance 
 {
 
+	/**
+	 * Given two strings, it returns true is the strings are one edit away
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	private static boolean isOneEditApart(String a, String b)
 	{
 	    if(Math.abs(a.length() - b.length()) > 1)
@@ -26,8 +33,41 @@ public class EditDistance
 	    return lenUnequal ? true : false;
 	}
 	
+	/**
+	 * Given two strings, it determines the minimum number of edits
+	 * to transform the string into another.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	private static int minEditDistance(String a, String b)
+	{
+	    int[][] distances = new int[a.length()+1][b.length()+1];
+	    for(int i = 0; i <= a.length(); i++)
+	    {
+	        for(int j = 0; j <= b.length(); j++)
+	        {
+	            if(i == 0 && j == 0)
+	                distances[0][0] = 0;
+
+	            else if(i == 0 && j > 0)
+	                distances[i][j] = 1 + distances[i][j-1];
+
+	            else if(i > 0 && j == 0)
+	                distances[i][j] = 1 + distances[i-1][j];
+	            else
+	            {
+	                int subCost = (a.charAt(i-1) == b.charAt(j-1)) ? distances[i-1][j-1] : (1 + distances[i-1][j-1]);
+	                distances[i][j] = Math.min(1+distances[i][j-1], 1+distances[i-1][j]);
+	                distances[i][j] = Math.min(subCost, distances[i][j]);
+	            }
+	        }
+	    }
+	    return distances[a.length()][b.length()];
+	}
+	
 	public static void main(String[] args)
 	{
-		System.out.print(isOneEditApart("xyz", "xy"));
+		System.out.print(minEditDistance("xyz", "yxz"));
 	}
 }
