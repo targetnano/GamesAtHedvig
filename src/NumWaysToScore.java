3a9d231769a8090845180669c1326e02e3805ee5
Kaviarasu Govindaraju <kavi@hedviginc.com>
2015-01-04 18:05:20 -0800

    Number of ways to get to the final score

---
+++ src/NumWaysToScore.java
@@ -0,0 +1,28 @@
+/**
+ * Given a set of scores and a final score, it finds
+ * the number of ways the team could get to that score.
+ * 
+ * @author Kavi
+ */
+public class NumWaysToScore {
+
+	private static int numWaysToScore(int[] scores, int targetScore)
+	{
+	    int[] numWays = new int[targetScore+1];
+	    numWays[0] = 1;
+	    for(int score : scores)
+	    {
+	        for(int i = score; i <= targetScore; i++)
+	        {
+	            numWays[i] += numWays[i-score];
+	        }
+	    }
+	    return numWays[targetScore];
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] scores = {2,3,7};
+		System.out.println(numWaysToScore(scores, 8));
+	}
+}
