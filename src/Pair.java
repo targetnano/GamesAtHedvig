e20e48b6e2ca89046e46e793fab41f660b75b731
Kaviarasu Govindaraju <kavi@hedviginc.com>
2015-01-05 16:41:57 -0800

    Adding pair util

---
+++ src/Pair.java
@@ -0,0 +1,36 @@
+
+public class Pair<K, V>
+{
+    private K k_;
+    private V v_;
+    
+    public Pair()
+    {
+    }
+
+    public Pair(K k, V v)
+    {
+        k_ = k;
+        v_ = v;
+    }
+    
+    public void k(K k)
+    {
+        k_ = k;
+    }
+
+    public K k()
+    {
+        return k_;
+    }
+    
+    public V v()
+    {
+        return v_;
+    }
+
+    public void v(V v)
+    {
+        v_ = v;
+    }
+}
