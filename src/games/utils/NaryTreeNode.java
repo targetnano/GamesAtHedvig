0e89b69818208d9f85c0636aad493a93aadd00bf
Kaviarasu Govindaraju <kavi@hedviginc.com>
2015-01-09 13:41:08 -0800

    N-ary tree node API

---
+++ src/games/utils/NaryTreeNode.java
@@ -0,0 +1,31 @@
+package games.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaryTreeNode 
+{
+
+	private int value_;
+	private List<NaryTreeNode> children_ = new ArrayList<NaryTreeNode>();
+	
+	public NaryTreeNode(int value)
+	{
+		value_ = value;
+	}
+	
+	public void addChild(NaryTreeNode child)
+	{
+		children_.add(child);
+	}
+	
+	public List<NaryTreeNode> getChildren()
+	{
+		return children_;
+	}
+	
+	public int value()
+	{
+		return value_;
+	}
+}
