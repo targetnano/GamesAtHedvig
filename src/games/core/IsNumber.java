ccdf090b65ec950c428eba83bdfb79acfb524abc
Kaviarasu Govindaraju <kavi@hedviginc.com>
2015-02-08 00:44:00 -0800

    Updating number validation

--- src/games/core/IsNumber.java
+++ src/games/core/IsNumber.java
@@ -1,11 +1,18 @@
 package games.core;
 
+/**
+ * http://fenghaolw.blogspot.com/2013/03/valid-number.html?view=sidebar
+ * 
+ * @author Kavi
+ *
+ */
 public class IsNumber 
 {
 
 	private static boolean isNumber(String number)
 	{
 	    int numDots = 0;
+	    int es = 0;
 
 	    for(int i = 0; i < number.length(); i++)
 	    {
@@ -14,9 +21,9 @@ public class IsNumber
 	            return false;
 	        if( ch >= '0' && ch <= '9')
 	            continue;
-	        else if(ch == '-')
+	        else if(ch == '-' || ch == '+')
 	        {
-	        	if(i != 0)
+	        	if(i != 0 && number.charAt(i-1) != 'e')
 	        		return false;
 	        }
 	        else if(ch == '.')
@@ -24,6 +31,11 @@ public class IsNumber
 	            numDots++;
 	            if(i == 1 && number.charAt(0) == '-')
 	                return false;
+	        }
+	        else if(ch == 'e')
+	        {
+	        	es++;
+	        	if(es > 1) return false;
 	        }
 	        else 
 	        	return false;
